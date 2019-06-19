6baccd8ab66e2f60e812373c52d4c024ff5f5b62
L.R.L <DaleLoreNY@gmail.com>
2019-06-19 15:41:02 -0400

    CRUD-ed spells model

--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -9,7 +9,35 @@ class SpellsController < ApplicationController
     @spell = Spell.find(params[:id])
   end
 
+  def new
+    @spell = Spell.new
+  end
 
+  def create
+    @spell = Spell.create(spells_params)
+    redirect_to @spell
+  end
 
+  def edit
+    @spell = Spell.find(params[:id])
+  end
+
+  def update
+    @spell = Spell.find(params[:id])
+    @spell.update(spells_params)
+    redirect_to spell
+  end
+
+  def destroy
+    @spell = Spell.find(params[:id])
+    @spell.destroy
+    redirect_to spells_path
+  end
+
+private
+
+  def spells_params
+    params.require(:spell).permit(:name, :category, :interactive, :magical_supplies, :instructions)
+  end
 
 end
