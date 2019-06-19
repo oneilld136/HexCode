1cf08c3c2d5b03959efb689b81004d3e5a08e32e
L.R.L <DaleLoreNY@gmail.com>
2019-06-19 14:29:49 -0400

    Fleshed out the spells model

--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -1,15 +1,15 @@
 class SpellsController < ApplicationController
 
 
-def index
-  @spells = Spell.all
-end
+  def index
+    @spells = Spell.all
+  end
 
   def show
     @spell = Spell.find(params[:id])
   end
 
-  
+
 
 
 end
